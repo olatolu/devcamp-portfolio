7ea3aff1837d991ca6386870bc23da5dce495d65
Olateru Olu <oluolateru@gmail.com>
2021-03-01 23:50:42 +0100

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,4 +5,22 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  #Portfolion_item New
+  def new
+    @portfolio_item = Portfolio.new
+
+  end
+
+  # POST /portfolio_item
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: "Portfolio_item was successfully created." }
+      end
+
+    end
+  end
+
 end
